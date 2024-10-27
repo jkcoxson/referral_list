661faaa5e63b6241a2a7e5254ffd4b5fe447f2c7
Jackson Coxson <admin@jkcoxson.com>
2024-10-26 21:55:47 -0700

    Implement sending list to Holly

--- src/holly/mod.rs
+++ src/holly/mod.rs
@@ -1,22 +1,29 @@
 // Jackson Coxson
 
-use std::{path::PathBuf, str::FromStr};
-
-use chrono::NaiveDate;
-use log::{error, info, warn};
+use log::{error, info};
 use serde::{Deserialize, Serialize};
-use tokio::{io::AsyncReadExt, sync::mpsc::UnboundedSender, time::sleep_until};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    sync::mpsc::UnboundedSender,
+    time::sleep_until,
+};
 
 use crate::church::ChurchClient;
 
 pub mod config;
 mod send_time;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Default, Serialize, Deserialize)]
 pub struct Message {
     sender: String,
     content: String,
     chat_id: String,
+}
+
+impl Message {
+    fn to_bytes(&self) -> Vec<u8> {
+        serde_json::to_vec(self).unwrap()
+    }
 }
 
 pub async fn main(church_client: &mut ChurchClient) -> anyhow::Result<()> {
@@ -31,8 +38,7 @@ pub async fn main(church_client: &mut ChurchClient) -> anyhow::Result<()> {
 
     loop {
         let mut stream = tokio::net::TcpStream::connect(&holly_config.holly_socket).await?;
-        let mut next_time_check =
-            tokio::time::Instant::now() + tokio::time::Duration::from_secs(20);
+        let mut next_time_check = tokio::time::Instant::now() + tokio::time::Duration::from_secs(1);
         if loop {
             let mut buf = [0u8; 1024 * 8];
             tokio::select! {
@@ -69,6 +75,35 @@ pub async fn main(church_client: &mut ChurchClient) -> anyhow::Result<()> {
                     let mut st = send_time::SendTime::load(&church_client.env).await?;
                     if st.is_go_time().await? {
                         info!("Sending Holly's list!");
+                        let report = if let Some(report) = crate::report::Report::read_report(&church_client.env)? {
+                            report
+                        } else {
+                            crate::generate_report(church_client).await?
+                        };
+
+                        let contacts = crate::get_average(church_client).await?;
+                        let mut contacts = contacts.into_iter().collect::<Vec<(String, usize)>>();
+                        contacts.sort_unstable_by(|a, b| a.1.cmp(&b.1));
+
+                        let mut avg_report = "".to_string();
+                        for (k, v) in contacts {
+                            if let Some(bl) = &holly_config.blacklist {
+                                if bl.contains(&k) {
+                                    continue;
+                                }
+                            }
+                            avg_report = format!("{avg_report}\n{k}: {v}");
+                        }
+                        for (zone_id, chat_id) in &holly_config.zone_chats {
+                            let msg = if let Some(p) = report.get_pretty_zone(zone_id) {
+                                format!("Average Contact Time:\n{avg_report}\n\nUncontacted Referrals:\n\n{p}")
+                            } else {
+                                info!("No uncontacted referrals in {zone_id}");
+                                format!("Average Contact Time:{avg_report}\n\nNo uncontacted referrals! Great work!")
+                            };
+                            info!("Sending {msg} to {chat_id}");
+                            stream.write_all(&Message { content: msg, chat_id: chat_id.to_string(), ..Default::default() }.to_bytes()).await?;
+                        }
                     }
                 }
                 _ = rx.recv() => {
